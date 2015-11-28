e22d62d84e9fe518a04cb0af89c589be3c3f01a2
Donovan Ong <donovan.ongyl@gmail.com>
2015-11-28 22:14:55 +0000

    Set environment settings to suit localhost

--- app/env_settings_example.py
+++ app/env_settings_example.py
@@ -7,19 +7,19 @@ import os
 # The settings below can (and should) be over-ruled by OS environment variable settings
 
 # Flask settings                     # Generated with: import os; os.urandom(24)
-SECRET_KEY = '\xb9\x8d\xb5\xc2\xc4Q\xe7\x8ej\xe0\x05\xf3\xa3kp\x99l\xe7\xf2i\x00\xb1-\xcd'
+SECRET_KEY = '\x9d|*\xbb\x82T\x83\xeb\xf52\xd1\xdfl\x87\xb4\x9e\x10f\xdf\x9e\xea\xf8_\x99'
 # PLEASE USE A DIFFERENT KEY FOR PRODUCTION ENVIRONMENTS!
 
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
 
 # Flask-Mail settings
-MAIL_USERNAME = 'email@example.com'
-MAIL_PASSWORD = 'password'
-MAIL_DEFAULT_SENDER = '"AppName" <noreply@example.com>'
-MAIL_SERVER = 'MAIL_SERVER', 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_SSL = True
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_DEFAULT_SENDER = ''
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USE_SSL = False
 MAIL_USE_TLS = False
 
 ADMINS = [
