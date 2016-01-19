bd169d860ddb31826252f70e410e72153be858b9
stupidjc <stupidjc@Johnsons-MacBook-Pro.local>
2016-01-19 17:54:10 +0000

    install flask & extensions
    bash shell file to automate installation of flask & required extensions
    of flask

---
+++ installflask.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install required flask & extensions
+pip install flask
+pip install flask-script
+pip install flask-user
+pip install flask-migrate
+pip install flask-mail
+pip install flask-sqlalchemy
