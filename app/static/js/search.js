869bb53f4688ed6769c35889de2986dfc7b0a956
Donovan Ong <donovan.ongyl@gmail.com>
2016-02-10 20:54:54 +0000

    Implement search form on home page

---
+++ app/static/js/search.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#movie").autocomplete({
+
+        source: function(request, response) {
+            $.getJSON($SCRIPT_ROOT + "/_search_movie", 
+                { term: request.term }, 
+                response);
+        },
+        minLength: 2,
+        messages: {
+            noResults: '',
+            results: function() {}
+        }
+
+    });
+});
