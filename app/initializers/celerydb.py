b045b76c06cb49530a28f0059ef840d5e86a4fba
Donovan Ong <donovan.ongyl@gmail.com>
2016-03-28 14:50:36 +0100

    Remove unused import

--- app/initializers/celerydb.py
+++ app/initializers/celerydb.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-from app.initializers import settings
 
 engine = create_engine(
     'sqlite:///app.sqlite', convert_unicode=True,
