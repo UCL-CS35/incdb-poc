080f661ebe9183c2d52efd902ceb516e987f9ee6
Donovan Ong <donovan.ongyl@gmail.com>
2016-03-30 12:24:05 +0100

    Unit Test Cases

--- tests/test_page_urls.py
+++ tests/test_page_urls.py
@@ -8,35 +8,35 @@ from flask import url_for
 
 def test_page_urls(client):
     # Visit home page
-    response = client.get(url_for('core.home_page'))
-    assert b'<h1>Home page</h1>' in response.data
+    response = client.get(url_for('home.index'))
+    assert b'<h1>Internet Neurocinematics Database</h1>' in response.data
 
     # Login as user and visit User page
     response = client.post(url_for('user.login'), follow_redirects=True,
-                           data=dict(email='user@example.com', password='Password1'))
-    assert b'<h1>Home page</h1>' in response.data
-    response = client.get(url_for('core.user_page'))
-    assert b'<h1>User page</h1>' in response.data
+                           data=dict(email='jeremy@incdb.com', password='Password1'))
+    assert b'<h1>Internet Neurocinematics Database</h1>' in response.data
+    response = client.get(url_for('user.user_account'))
+    assert b'<h1>Account</h1>' in response.data
 
-    # Edit User Profile page
-    response = client.get(url_for('core.user_profile_page'))
-    assert b'<h1>User Profile</h1>' in response.data
-    response = client.post(url_for('core.user_profile_page'), follow_redirects=True,
-                           data=dict(first_name='User', last_name='User'))
-    response = client.get(url_for('core.user_page'))
-    assert b'<h1>User page</h1>' in response.data
+    # # Edit User Profile page
+    response = client.get(url_for('user.user_account'))
+    assert b'<h1>Account</h1>' in response.data
+    response = client.post(url_for('user.user_account'), follow_redirects=True,
+                           data=dict(first_name='new first name', last_name='User'))
+    response = client.get(url_for('user.user_account'))
+    assert b'new first name' in response.data
 
-    # Logout
+    # # Logout
     response = client.get(url_for('user.logout'), follow_redirects=True)
-    assert b'<h1>Home page</h1>' in response.data
+    assert b'<h1>Internet Neurocinematics Database</h1>' in response.data
 
-    # Login as admin and visit Admin page
+    # # Login as admin and visit Admin page
     response = client.post(url_for('user.login'), follow_redirects=True,
-                           data=dict(email='admin@example.com', password='Password1'))
-    assert b'<h1>Home page</h1>' in response.data
-    response = client.get(url_for('core.admin_page'))
-    assert b'<h1>Admin page</h1>' in response.data
+                           data=dict(email='jeremy@incdb.com', password='Password1'))
+    assert b'<h1>Internet Neurocinematics Database</h1>' in response.data
+    response = client.get(url_for('admin.index'))
+    assert b'<h1>Manage INcDb</h1>' in response.data
 
-    # Logout
+    # # Logout
     response = client.get(url_for('user.logout'), follow_redirects=True)
-    assert b'<h1>Home page</h1>' in response.data
+    assert b'<h1>Internet Neurocinematics Database</h1>' in response.data
--- tests/test_user_management.py
+++ tests/test_user_management.py
@@ -1,33 +1,33 @@
 from __future__ import print_function  # Use print() instead of print
-from app.core.models import User
+from app.models.users import User, Role
 from app import db
-from app.startup.create_users import find_or_create_user
+from app.initializers.create_users import find_or_create_user
+
 
 def test_user_management(client):
 
-	# allows user to register
-	user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
-	assert user
+    # allows user to register
+    user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
+    assert user
 
-	# allows user to reset password 
-	new_user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
-	new_user.password = u'Password2'
-	db.session.commit()
-	user = User.query.filter(User.email == u'user2@example.com').first()
-	assert user.password == 'Password2'
+    # allows user to reset password 
+    new_user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
+    new_user.password = u'Password2'
+    db.session.commit()
+    user = User.query.filter(User.email == u'user2@example.com').first()
+    assert user.password == 'Password2'
 
-	# allow user to edit title
-	new_user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
-	new_user.title = u'Dr'
-	db.session.commit()
-	user = User.query.filter(User.email == u'user2@example.com').first()
-	assert user.title == 'Dr'
+    # allow user to edit title
+    new_user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
+    new_user.title = u'Dr'
+    db.session.commit()
+    user = User.query.filter(User.email == u'user2@example.com').first()
+    assert user.title == 'Dr'
 
-	# allow user to edit first_name and last_name
-	new_user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
-	new_user.first_name = u'Jeremy'
-	new_user.last_name = u'Skipper'
-	db.session.commit()
-	user = User.query.filter(User.email == u'user2@example.com').first()
-	assert (user.first_name == 'Jeremy' and user.last_name == 'Skipper')
-
+    # allow user to edit first_name and last_name
+    new_user = find_or_create_user(u'User2', u'Example', u'Mr', u'user2@example.com', 'Password1')
+    new_user.first_name = u'Jeremy'
+    new_user.last_name = u'Skipper'
+    db.session.commit()
+    user = User.query.filter(User.email == u'user2@example.com').first()
+    assert (user.first_name == 'Jeremy' and user.last_name == 'Skipper')
