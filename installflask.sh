6ec2a76db2b4759a76e81570cd1334de2d37726e
deploy-incdb <incdb-ucl@gmail.com>
2016-04-27 11:03:36 -0400

    Remove Procfile

--- installflask.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install required flask & extensions
-pip install flask
-pip install flask-script
-pip install flask-user
-pip install flask-migrate
-pip install flask-mail
-pip install flask-sqlalchemy
